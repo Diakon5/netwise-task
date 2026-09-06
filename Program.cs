5a701d1854efdeec353e87607c31ed7e2908d2db
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2026-09-06 15:10:53 +0200

    Add basic program functionality

--- Program.cs
+++ Program.cs
@@ -1,9 +1,33 @@
-﻿namespace netwise_task;
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace netwise_task;
 
 class Program
 {
+    private static readonly HttpClient httpClient = new()
+    {
+        BaseAddress = new Uri("https://catfact.ninja"),
+    };
+    private static async Task Run()
+    {
+        using HttpResponseMessage response = await httpClient.GetAsync("fact");
+        response.EnsureSuccessStatusCode();
+        string jsonResponse = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"{jsonResponse}\n");
+        if(!jsonResponse.EndsWith('\n'))
+        {
+            jsonResponse = $"{jsonResponse}\n";
+        }
+        File.AppendAllText("catfact.txt",jsonResponse);
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("If you can see this, the app is incomplete");
+        Run().Wait();
+        
     }
 }
